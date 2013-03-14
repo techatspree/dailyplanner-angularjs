d3dff820bd8196dfa608e778be945e2068b157d7
Till Hermsen <till.hermsen@akquinet.de>
2013-03-14 14:26:59 +0100

    Edit task title

--- app/js/controller.js
+++ app/js/controller.js
@@ -2,17 +2,19 @@ function TaskListController($scope) {
     var tasks = $scope.tasks = [];
 
     $scope.addTask = function(newTask) {
-        var title;
+        var title, duration;
 
         if (!newTask) { return; }
 
         title = newTask;
+        duration = newTask.duration || 0;
         
         tasks.push({
-            "title": title,
-            "duration": 0
+            title: title,
+            duration: duration
         });
 
         $scope.newTask = null;
     };
+
 }
