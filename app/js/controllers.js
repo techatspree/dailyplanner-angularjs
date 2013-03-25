367add78e43416262309c0a04e32355242c742d6
Till Hermsen <till.hermsen@akquinet.de>
2013-03-25 10:54:02 +0100

    Refactoring

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,11 +11,10 @@ function TaskListController($scope) {
 
         title = self.getTitle(self.matchPattern, newTask.toString());
         duration = self.getDuration(self.matchPattern, newTask.toString());
-        
+
         tasks.push({
             title: title,
             duration: duration,
-            class: "",
             done: false
         });
 
@@ -49,45 +48,45 @@ function TaskListController($scope) {
 
     $scope.toggleTaskStatus = function(task) {
         task.done = !task.done;
-        task.class =  (task.done) ? "task-done" : "";
     };
-
-    // Prototype functions
-
-    TaskListController.prototype.matchPattern = function (str) {
-        return str.match(/(\s*[0-9]+h)?(\s*[0-9]+m)?$/);
-    };
-
-    TaskListController.prototype.getTitle = function(matcher, str) {
-        var matchedInput, title;
-
-        matchedInput = matcher(str);
-
-        title = (matchedInput[0])
-            ? matchedInput.input.replace(matchedInput[0], "")
-            : matchedInput.input;
-            
-        return title;
-    };
-
-    TaskListController.prototype.getDuration = function(matcher, str) {
-        var matchedInput, duration;
-
-        matchedInput = matcher(str);
-        
-        duration = 0;
-
-        if (matchedInput[1]) {
-            var h = parseInt(matchedInput[1].replace(/\s|h/, ""));
-            duration = h * 60;
-        }
-
-        if (matchedInput[2]) {
-            var m = parseInt(matchedInput[2].replace(/\s|m/, ""));
-            duration += m;
-        }
-
-        return duration;
-    };
-
 }
+
+TaskListController.$inject = ["$scope"];
+
+
+// Prototype functions
+TaskListController.prototype.matchPattern = function (str) {
+    return str.match(/(\s*[0-9]+h)?(\s*[0-9]+m)?$/);
+};
+
+TaskListController.prototype.getTitle = function(matcher, str) {
+    var matchedInput, title;
+
+    matchedInput = matcher(str);
+
+    title = (matchedInput[0])
+        ? matchedInput.input.replace(matchedInput[0], "")
+        : matchedInput.input;
+
+    return title;
+};
+
+TaskListController.prototype.getDuration = function(matcher, str) {
+    var matchedInput, duration, h, m;
+
+    matchedInput = matcher(str);
+
+    duration = 0;
+
+    if (matchedInput[1]) {
+        h = parseInt(matchedInput[1].replace(/\s|h/, ""), 10);
+        duration = h * 60;
+    }
+
+    if (matchedInput[2]) {
+        m = parseInt(matchedInput[2].replace(/\s|m/, ""), 10);
+        duration += m;
+    }
+
+    return duration;
+};
