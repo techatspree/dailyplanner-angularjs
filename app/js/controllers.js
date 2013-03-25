cb6fbf51c17e092e80f145721c89b2024311ab16
Till Hermsen <till.hermsen@akquinet.de>
2013-03-25 10:20:43 +0100

    Added "use strict" to controller.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,18 @@
+"use strict";
+
 function TaskListController($scope) {
     $scope.tasks = [
         {
             title: "Task 1",
-            duration: 30,
+            duration: 30
         },
         {
             title: "Task 2",
-            duration: 20,
+            duration: 20
         },
         {
             title: "Task 3",
-            duration: 15,
+            duration: 15
         }
     ];
 }
