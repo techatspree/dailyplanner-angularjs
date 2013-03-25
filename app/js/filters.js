ed1cbd84e830c108983d659718e35083e3142481
Till Hermsen <till.hermsen@akquinet.de>
2013-03-25 16:13:59 +0100

    Added duration filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,6 +5,34 @@
 (function(angular) {
     "use strict";
 
-    angular.module("Filters", []);
+    angular.module("Filters", []).
+
+        filter("DurationFormat", [
+            function() {
+                return function(input) {
+                    var output, hours, minutes;
+                    output = "";
+
+                    if (input >= 60) {
+                        hours = parseInt(input / 60, 10);
+                    }
+
+                    if (input > 0) {
+                        minutes = input % 60;
+                    }
+
+                    if (hours) {
+                        output = hours + "h";
+                    }
+
+                    if (minutes) {
+                        if (output) { output += " "; }
+                        output += minutes + "m";
+                    }
+
+                    return output;
+                };
+            }
+        ]);
 
 }(angular));
