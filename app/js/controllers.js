d12c699f8c3b7b4df2b9183ff04cdbd76f0cd861
Till Hermsen <till.hermsen@akquinet.de>
2013-03-27 13:06:38 +0100

    Redesign

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,17 +4,24 @@ function TaskListController($scope) {
     $scope.tasks = [
         {
             title: "Task 1",
-            duration: 30
+            description: "Beschreibung Task 1 ...",
+            duration: 30,
+            done: false
         },
         {
             title: "Task 2",
-            duration: 20
+            description: "Beschreibung Task 2 ...",
+            duration: 20,
+            done: true
         },
         {
             title: "Task 3",
-            duration: 15
+            description: "Beschreibung Task 3 ...",
+            duration: 15,
+            done: false
         }
     ];
 }
 
+// inject needed services
 TaskListController.$inject = ["$scope"];
