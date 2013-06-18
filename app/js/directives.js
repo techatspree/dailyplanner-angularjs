2f334b5692b84345074a7f3de81dbb48dcb349b4
Till Hermsen <till.hermsen@akquinet.de>
2013-06-18 14:34:48 +0200

    added task directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,5 +5,21 @@
 (function(angular) {
     "use strict";
 
-    angular.module("directives", []);
+    angular.module("directives", []).
+
+        directive("task", [
+            function() {
+                return {
+                    restrict: "A",
+                    scope: {
+                        task: "=",
+                        toggleTaskStatus: "&",
+                        deleteTask: "&"
+                    },
+                    templateUrl: "partials/task.html",
+                    replace: true,
+                    link: function(scope, element, attrs) {}
+                };
+            }
+        ]);
 }(angular));
