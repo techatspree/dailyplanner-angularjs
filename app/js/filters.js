5170f4567406b1010cb301af04956e4ed6a921d8
Till Hermsen <till.hermsen@akquinet.de>
2013-06-20 11:03:52 +0200

    added truncateCharacters Filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -14,15 +14,33 @@
 
                     if (isNaN(input) || input === 0) { return; }
 
-                    hours = (input >= 60) ? parseInt(input / 60, 10).toString() : "0";
-                    output = (input < 600) ? "0" + hours : hours;
+                    hours = parseInt(input / 60, 10);
+                    output = (input < 600) ? "0" + hours.toString() : hours.toString();
                     output += "h ";
 
-                    minutes = (input % 60).toString();
-                    output += (input % 60 < 10) ? "0" + minutes : minutes;
+                    minutes = input % 60;
+                    output += (input % 60 < 10) ? "0" + minutes.toString() : minutes.toString();
                     output += "m";
 
                     return output;
+                };
+            }
+        ]).
+
+        filter("truncateCharacters", [
+            function() {
+                return function(input, chars) {
+                    if (isNaN(chars)) { return input; }
+
+                    if (chars <= 0) { return ""; }
+
+                    if (input && input.length >= chars) {
+                        input = input.substring(0, chars);
+
+                        return input + "...";
+                    }
+
+                    return input;
                 };
             }
         ]);
