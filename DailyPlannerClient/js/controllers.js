32cbd6717331dbed5ffe71178233b061cf5a9467
Till Hermsen <till.hermsen@akquinet.de>
2013-07-16 15:54:21 +0200

    updated step 04

---
+++ DailyPlannerClient/js/controllers.js
@@ -0,0 +1,46 @@
+/*global
+    angular
+ */
+
+(function (angular) {
+    "use strict";
+
+    angular.module("controllers", []).
+
+        controller("taskListController", [
+            "$scope",
+            "localTaskStorage",
+
+            function ($scope, storage) {
+                $scope.tasks = storage;
+                storage.fetchTasks();
+
+                $scope.addNewTask = function (newTaskTitle) {
+                    var newTask;
+
+                    if (!newTaskTitle) { return; }
+
+                    newTask = {
+                        title: newTaskTitle,
+                        description: "",
+                        duration: 0,
+                        done: false
+                    };
+
+                    $scope.newTaskTitle = null;
+                    storage.addNewTask(newTask);
+                    storage.synchronize();
+                };
+
+                $scope.deleteTask = function (taskIndex) {
+                    storage.deleteTask(taskIndex);
+                    storage.synchronize();
+                };
+
+                $scope.toggleTaskStatus = function (task) {
+                    task.done = !task.done;
+                    storage.synchronize();
+                };
+            }
+        ]);
+}(angular));
---
+++ DailyPlannerClient/test/unit/spec/controller-spec.js
@@ -0,0 +1,76 @@
+/*global
+    jasmine,
+    describe, it, expect,
+    beforeEach, afterEach,
+    module, inject,
+    angular
+ */
+
+(function (jasmine, describe, it, expect, beforeEach, afterEach, module, inject, angular) {
+    "use strict";
+
+    describe("controllers", function () {
+        describe("taskListController", function () {
+            var scope, controller, taskStorageMock, data;
+
+            beforeEach(module("controllers"));
+
+            beforeEach(inject(function ($rootScope, $controller) {
+                scope = $rootScope.$new();
+
+                taskStorageMock = (function () {
+                    return {
+                        data: [],
+                        synchronize: function () {},
+                        fetchTasks: function () {},
+                        addNewTask: function (newTask) {
+                            this.data.push(newTask);
+                        },
+                        deleteTask: function (taskIndex) {
+                            this.data.splice(taskIndex, 1);
+                        }
+                    };
+                }());
+
+                controller = $controller("taskListController", {
+                    $scope: scope,
+                    localTaskStorage: taskStorageMock
+                });
+            }));
+
+            beforeEach(function () {
+                data = {};
+                data.newTaskTitle = "new task";
+                data.task = {
+                    title: "new task",
+                    description: "description",
+                    duration: 15,
+                    done: false
+                };
+            });
+
+            it("should add a new task", function () {
+                expect(scope.tasks.data.length).toEqual(0);
+                scope.addNewTask(data.newTaskTitle);
+                expect(scope.tasks.data.length).toEqual(1);
+            });
+
+            it("should remove a task", function () {
+                scope.tasks.data.push(data.task);
+                expect(scope.tasks.data.indexOf(data.task)).toEqual(0);
+
+                scope.deleteTask(scope.tasks.data.indexOf(data.task));
+                expect(scope.tasks.data.indexOf(data.task)).toEqual(-1);
+            });
+
+            it("should set a task done", function () {
+                scope.tasks.data.push(data.task);
+                expect(scope.tasks.data.length).toEqual(1);
+                expect(scope.tasks.data[scope.tasks.data.indexOf(data.task)].done).toEqual(false);
+
+                scope.toggleTaskStatus(data.task);
+                expect(scope.tasks.data[scope.tasks.data.indexOf(data.task)].done).toEqual(true);
+            });
+        });
+    });
+}(jasmine, describe, it, expect, beforeEach, afterEach, module, inject, angular));
