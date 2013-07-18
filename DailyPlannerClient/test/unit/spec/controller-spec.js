64eddbd71644fb90b0dddda872e34b147c4135e6
Till Hermsen <till.hermsen@akquinet.de>
2013-07-18 09:35:17 +0200

    cleanup

--- DailyPlannerClient/test/unit/spec/controller-spec.js
+++ DailyPlannerClient/test/unit/spec/controller-spec.js
@@ -9,48 +9,52 @@
 (function (jasmine, describe, it, expect, beforeEach, afterEach, module, inject, angular) {
     "use strict";
 
-    describe("taskListController", function () {
-        var scope, controller, data;
+    describe("controllers", function () {
 
-        beforeEach(inject(function ($rootScope, $controller) {
-            scope = $rootScope.$new();
-            controller = $controller(taskListController, { $scope: scope });
-        }));
+        describe("taskListController", function () {
+            var scope, controller, data;
 
-        beforeEach(function () {
-            data = {};
-            data.newTaskTitle = "new task";
-            data.task = {
-                title: "new task",
-                description: "description",
-                duration: 15,
-                done: false
-            };
+            beforeEach(inject(function ($rootScope, $controller) {
+                scope = $rootScope.$new();
+                controller = $controller(taskListController, { $scope: scope });
+            }));
+
+            beforeEach(function () {
+                data = {};
+                data.newTaskTitle = "new task";
+                data.task = {
+                    title: "new task",
+                    description: "description",
+                    duration: 15,
+                    done: false
+                };
+            });
+
+
+            it("should add a new task", function () {
+                expect(scope.tasks.length).toEqual(0);
+
+                scope.addNewTask(data.newTaskTitle);
+                expect(scope.tasks.length).toEqual(1);
+            });
+
+            it("should remove a task", function () {
+                scope.tasks.push(data.task);
+                expect(scope.tasks.indexOf(data.task)).toEqual(0);
+
+                scope.deleteTask(0);
+                expect(scope.tasks.indexOf(data.task)).toEqual(-1);
+            });
+
+            it("should set a task done", function () {
+                scope.tasks.push(data.task);
+                expect(scope.tasks.indexOf(data.task)).toEqual(0);
+                expect(scope.tasks[0].done).toEqual(false);
+
+                scope.toggleTaskStatus(scope.tasks[0]);
+                expect(scope.tasks[0].done).toEqual(true);
+            });
         });
 
-
-        it("should add a new task", function () {
-            expect(scope.tasks.length).toEqual(0);
-
-            scope.addNewTask(data.newTaskTitle);
-            expect(scope.tasks.length).toEqual(1);
-        });
-
-        it("should remove a task", function () {
-            scope.tasks.push(data.task);
-            expect(scope.tasks.indexOf(data.task)).toEqual(0);
-
-            scope.deleteTask(0);
-            expect(scope.tasks.indexOf(data.task)).toEqual(-1);
-        });
-
-        it("should set a task done", function () {
-            scope.tasks.push(data.task);
-            expect(scope.tasks.indexOf(data.task)).toEqual(0);
-            expect(scope.tasks[0].done).toEqual(false);
-
-            scope.toggleTaskStatus(scope.tasks[0]);
-            expect(scope.tasks[0].done).toEqual(true);
-        });
     });
 }(jasmine, describe, it, expect, beforeEach, afterEach, module, inject, angular));
