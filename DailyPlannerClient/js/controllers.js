5ca0c3279e36ffa0117de73af2aa895482aa3e6e
Till Hermsen <till.hermsen@akquinet.de>
2013-09-09 14:50:09 +0200

    updated step-01

--- DailyPlannerClient/js/controllers.js
+++ DailyPlannerClient/js/controllers.js
@@ -4,7 +4,7 @@ function taskListController($scope) {
     $scope.tasks = [
         {
             title: "Task 1",
-            description: "Task1 Beschreibung",
+            description: "Task 1 Beschreibung",
             duration: 120,
             done: false
         },
@@ -15,8 +15,8 @@ function taskListController($scope) {
             done: false
         },
         {
-            title: "Task 2",
-            description: "Task3 Beschreibung",
+            title: "Task 3",
+            description: "Task 3 Beschreibung",
             duration: 40,
             done: false
         }
