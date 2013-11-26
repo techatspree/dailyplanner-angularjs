7f5eb130136c8c2cb0c94ebc7a2f8235b53eb3da
Till Hermsen <till.hermsen@akquinet.de>
2013-11-26 11:44:32 +0100

    updated step-04

--- DailyPlannerClient/test/unit/spec/controllers-spec.js
+++ DailyPlannerClient/test/unit/spec/controllers-spec.js
@@ -53,6 +53,7 @@
 
                 scope.newTaskTitle = testData.newTaskTitle;
                 scope.addNewTask();
+                expect(scope.newTaskTitle).toEqual(null);
                 expect(scope.tasks.length).toEqual(1);
             });
 
