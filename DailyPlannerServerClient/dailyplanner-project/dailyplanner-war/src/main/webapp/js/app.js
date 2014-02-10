8e29453471a4d52ef5bcaafa9158cf4d1a6a07c2
Till Hermsen <till.hermsen@akquinet.de>
2014-02-10 11:30:16 +0100

    httpInterceptor ersetzt (alte Implementierung war DEPRECATED)

--- DailyPlannerServerClient/dailyplanner-project/dailyplanner-war/src/main/webapp/js/app.js
+++ DailyPlannerServerClient/dailyplanner-project/dailyplanner-war/src/main/webapp/js/app.js
@@ -6,11 +6,11 @@
     "use strict";
 
     angular.module("dailyPlanner", [
-            "controllers",
-            "services",
-            "directives",
-            "filters"
-        ])
+        "controllers",
+        "services",
+        "directives",
+        "filters"
+    ])
 
         .factory("httpInterceptor", [
             "$q",
@@ -18,39 +18,35 @@
             "$log",
 
             function ($q, $window, $log) {
-                return function (promise) {
-                    return promise.then(
-                        function (response) {
-                            // success
-                            var responseHeader;
+                return {
+                    "response": function (response) {
+                        // do something on success
+                        var responseHeader;
+                        responseHeader = response.headers();
 
-                            responseHeader = response.headers();
-
-                            if (response.data && responseHeader["content-type"].indexOf("text/html") !== -1) {
-                                if (response.data.indexOf('<meta name="unauthorized" content="true">') !== -1) {
-                                    $log.error("unauthorized");
-                                    $window.location.reload();
-                                    return $q.reject(response);
-                                }
+                        if (response.data && responseHeader["content-type"].indexOf("text/html") !== -1) {
+                            if (response.data.indexOf('<meta name="unauthorized" content="true">') !== -1) {
+                                $log.error("unauthorized");
+                                $window.location.reload();
+                                return $q.reject(response);
                             }
-
-                            return response;
-                        },
-                        function (response) {
-                            // error
-                            return $q.reject(response);
                         }
-                    );
+                        return response || $q.when(response);
+                    },
+
+                    "responseError": function (rejection) {
+                        // do something on error
+                        return $q.reject(rejection);
+                    }
                 };
             }
-
         ])
 
         .config([
             "$httpProvider",
 
             function ($httpProvider) {
-                $httpProvider.responseInterceptors.push("httpInterceptor");
+                $httpProvider.interceptors.push("httpInterceptor");
             }
         ]);
 }(angular));
