3ed00ffb3aaf66cfb28aa1269d42a7ec18826033
Till Hermsen <till.hermsen@akquinet.de>
2014-02-26 14:50:25 +0100

    add test tasks

--- DailyPlannerServerClient/dailyplanner-project/dailyplanner-war/src/main/java/de/akquinet/dailyplanner/web/rest/DailyPlanSimpleStorage.java
+++ DailyPlannerServerClient/dailyplanner-project/dailyplanner-war/src/main/java/de/akquinet/dailyplanner/web/rest/DailyPlanSimpleStorage.java
@@ -5,7 +5,11 @@ import javax.ejb.Singleton;
 @Singleton
 public class DailyPlanSimpleStorage {
 
-    private String dailyPlan = "";
+    private String dailyPlan = "[" +
+        "{\"title\":\"Joggen\",\"description\":\"Lauf um dein Leben, Marty!\",\"duration\": 60,\"done\": false}," +
+        "{\"title\": \"Sushi essen\",\"description\": \"Nigiri, Maguro, Tamagoyaki\",\"duration\": 30,\"done\": false}," +
+        "{\"title\":\"AngularJS coden\",\"description\":\"AngularJS, the Superheroic JavaScript MVW Framework\",\"duration\": 300,\"done\": false}" +
+        "]";
 
     public String getDailyPlan() {
         return this.dailyPlan;
